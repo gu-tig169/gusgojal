8a22744d7f4895d8dbac8403eab1a3f79103b6d8
agojkovic <gusgojal@student.gu.se>
2020-11-20 12:55:18 +0100

    Lägg till och ta bort funkar, inte filtrering

---
+++ lib/SecondView.dart
@@ -0,0 +1,70 @@
+import 'package:firstapp/Model.dart';
+import 'package:flutter/material.dart';
+
+class SecondView extends StatelessWidget {
+
+  final myController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey,
+      appBar: AppBar(
+        backgroundColor: Colors.grey[600],
+        title: Text('Lägg till uppgifter'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.only(top: 10.0, left: 20.0, right: 20.0),
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.only(top:30.0),
+                child: TextField(
+                  controller: myController,
+                  textCapitalization: TextCapitalization.sentences,
+                  keyboardType: TextInputType.text,
+                  decoration: InputDecoration(
+                    hintText: 'Lägg till uppgift...',
+                    border: OutlineInputBorder(),
+    ),
+  ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 95.0, top: 20),
+                child: Row(
+                  children: [
+                    _addButton(context, myController),
+                    _textAdd(),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+
+Widget _addButton(context, TextEditingController myController) {
+  return IconButton(
+      iconSize: 40.0,
+      icon: Icon(Icons.add),
+      onPressed: () {
+        print('You pressed add');
+        Navigator.pop(
+            context, Item(checkBoxIs: false, thingsToDo: myController.text));
+      });
+}
+
+Widget _textAdd() {
+  return Text(
+    'Lägg till',
+    style: TextStyle(
+      fontSize: 20.0,
+    ),
+  );
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,162 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'MainView.dart';
+import 'Model.dart';
 
 void main() {
-  runApp(MyApp());
+  var state = Model();
+  runApp(ChangeNotifierProvider(
+      create: (context) => state, child:  MyApp()));
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData.dark(),
+      debugShowCheckedModeBanner: false,
       home: MainView(),
     );
   }
-}
-
-class MainView extends StatelessWidget {
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        backgroundColor: Colors.black,
-        title: Center(
-            child: Padding(
-          padding: const EdgeInsets.only(left: 55.0),
-          child: Text('Alex To-Do'),
-        )),
-        actions: [
-          _dropdownMenu(),
-             ],
-        
-      ),
-      body: ListView(children: [
-        _checkbox('Plugga'),
-        _checkbox('Skriva'),
-        _checkbox('Städa'),
-        _checkbox('Kolla TV'),
-        _checkbox('Sova'),
-        _checkbox('Handla mat'),
-        _checkbox('Meditera'),
-        _checkbox('Tvätta'),
-      ]),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        backgroundColor: Colors.black,
-        onPressed: () {
-          print('Knappen funkar');
-          Navigator.push(
-              context, MaterialPageRoute(builder: (context) => SecondView()));
-        },
-      ),
-    );
-  }
-}
-// Dropdownmeny som filtrerar vad som är gjort eller inte
-Widget _dropdownMenu() {
-  List<String> alt = ['All', 'Done', 'Not done'];
-
-  return Padding(
-    padding: const EdgeInsets.all(12.0),
-    child: PopupMenuButton<String>(
-      icon: Icon(Icons.tune),
-      onSelected: altAction,
-          itemBuilder: (BuildContext context) {
-            return alt.map((String alt) {
-              return PopupMenuItem(
-                value: alt,
-                child: Text(alt),
-              );
-            }).toList();
-          }
-        ),
-  );
-    }
-    
-    class SecondView extends StatelessWidget {
-      @override
-      Widget build(BuildContext context) {
-        return Scaffold(
-          appBar: AppBar(
-              backgroundColor: Colors.black,
-              title: Padding(
-                padding: const EdgeInsets.only(left: 50.0),
-                child: Text('Lägg till uppgifter'),
-              )),
-          body: Padding(
-            padding: const EdgeInsets.all(25.0),
-            child: Column(
-              children: [
-                _textField(),
-                Padding(
-                  padding: EdgeInsets.only(left: 105.0, top: 30),
-                  child: Row(children: [
-                    _plusIcon(),
-                    _textBox(),
-                  ]),
-                ),
-              ],
-            ),
-          ),
-        );
-      }
-    }
-    
-    Widget _textBox() {
-      return Text('Lägg till');
-    }
-    
-    Widget _plusIcon() {
-      return IconButton(
-        icon: Icon(Icons.add),
-        onPressed: () {},
-      );
-    }
-    
-    Widget _textField() {
-      return TextField(
-          decoration: InputDecoration(
-              hintText: 'Lägg till uppgift', border: const OutlineInputBorder()));
-    }
-    
-    Widget _checkbox(String toDo) {
-      return Card(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(15)
-        ),
-              child: CheckboxListTile(
-            title: Text(
-              toDo,
-              style: TextStyle(
-                fontSize: 20.0 ),
-                ),
-                controlAffinity: ListTileControlAffinity.leading,
-                secondary: IconButton(
-                  icon: Icon(Icons.delete),
-                  onPressed: () {} , //Kod för ta bort saker från listan
-                  ),
-            value: false,
-            onChanged: (value) {
-              print('Hej');
-
-
-
-              
-
-            
-            
-            }),
-            
-      );
-    }
-    
-    void altAction(String alt) {
-      if (alt == 'All') {
-        print('You pressed: All');
-        } 
-        else if(alt == 'Done') {
-          print('You pressed Done');
-        } 
-        else if(alt == 'Not done') {
-          print('You pressed not done');
-        }
 }
